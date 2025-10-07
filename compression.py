6c413c094d8ac6bd5f5986c5ae9e25d50400ab37
Jack Johnson <jrj221@byu.edu>
2025-10-07 00:03:45 -0600

    Basic mapping algorithm complete. Plan to add in stringified pixel instead of the object itself. Perhaps compressString can be changed to be smaller but still parseable

--- compression.py
+++ compression.py
@@ -1,6 +1,32 @@
+from PIL import Image
+
 def main():
-    pass
+    image = Image.open("colorBox.png")
+    numColors = len(image.getcolors())
+    keys = []
+    for i in range(numColors):
+        keys.append(str(i))
+    map = {}
+    k = 0
+    compressString = ""
+    for i in range(10):
+        for j in range(10):
+            pixel = image.getpixel((i, j))
+            if not any(pixel in pixelList for pixelList in map.values()): # pixel doesn't exist in map
+                map[keys[k]] = [pixel]
+                compressString += keys[k] + ','
+                k = k + 1
+            else: # pixel DOES exist in map
+                for key in map:
+                    for item in map[key]:
+                        if item == pixel:
+                            compressString += key + ','
 
 
-if "name" == "__main__":
+
+    print(map)
+    print(compressString)
+
+
+if __name__ == "__main__":
     main()
