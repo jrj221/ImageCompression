b63acc56a1cd8a6f384b6d37b14604c2ff8860e0
Jack Johnson <jrj221@byu.edu>
2025-10-08 22:06:58 -0600

    Got rid of compression string and dictionary since they were entirely uneffective. Simply rounding the pixels to some multiple and recreating the image is efficient on its own

--- compression.py
+++ compression.py
@@ -1,58 +1,38 @@
-import sys
-import decompression
 from PIL import Image
 
 
-def get_compression_string(image, pixel_dict):
-    compressed_string = ''
-    pixels_count = len(image.getcolors(maxcolors=(image.width * image.height)))  # number of unique pixels
-    print("Number of unique pixels: " + str(pixels_count))
-    keys = []
-    for i in range(pixels_count):  # set up dict keys so each pixel can be replaced with a key
-        keys.append(str(i))
-
-    k = 0
+def get_rounded_pixels(image):
+    rounded_pixels = []
     for i in range(image.height):
         for j in range(image.width):  #iterate over image
-            print(i,j)
             pixel = image.getpixel((j, i))
-            rounding_multiple = 30
-            #pixel_dict takes form (r,g,b,a) = '1'
+            print(pixel)
+            rounding_multiple = 10
+
             rounded_pixel_red = rounding_multiple * round(pixel[0] / rounding_multiple)
             rounded_pixel_green = rounding_multiple * round(pixel[1] / rounding_multiple)
             rounded_pixel_blue = rounding_multiple * round(pixel[2] / rounding_multiple)
             rounded_pixel_alpha = rounding_multiple * round(pixel[3] / rounding_multiple)
             rounded_pixel = (rounded_pixel_red, rounded_pixel_green, rounded_pixel_blue, rounded_pixel_alpha)
-            if not rounded_pixel in pixel_dict:  # pixel doesn't exist in dict
-                pixel_dict[rounded_pixel] = keys[k]
-                compressed_string += keys[k] + ','
-                k += 1
-            else:  # pixel DOES exist in map
-                compressed_string += pixel_dict[rounded_pixel] + ','
+            rounded_pixels.append(rounded_pixel)
 
-    return compressed_string
+    return rounded_pixels
+
 
 def compress():
-    image = Image.open("world_map.png")
-
-    pixel_dict = {}
+    old_image = Image.open("transparent_cat.png")
+    print(old_image.mode)
     # create map of chars to pixels and write chars to a string
-    compressed_string = get_compression_string(image, pixel_dict)
+    rounded_pixels = get_rounded_pixels(old_image)
 
-    #print(pixel_dict)
-    #print("Compressed String: " + compressed_string)
-    print("Length of Compressed String: " + str(len(compressed_string)))
-
-    #write compressed_string to txt file
-    f = open("compressed.txt", "w")
-    f.write(compressed_string)
-    f.close()
-    f2 = open("pixel_dict.txt", "w")
-    f2.write(str(pixel_dict))
-    f2.close()
-    print("Length of Dictionary: " + str(len(pixel_dict)))
+    new_image = Image.new("RGBA", (old_image.width, old_image.height))
+    k = 0
+    for i in range(new_image.height):
+        for j in range(new_image.width):
+            new_image.putpixel((j, i), rounded_pixels[k])
+            k += 1
+    new_image.save("new_image.png")
 
 
 if __name__ == "__main__":
     compress()
-    decompression.decompress()
